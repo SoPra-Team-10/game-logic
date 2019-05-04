152d041feb20bc28743d6b081749c8093fae1a17
Paul Nykiel <paul.nykiel@gmail.com>
2019-05-04 15:10:47 +0200

    Update GameModel.cpp
    Co-Authored-By: jonas-merkle <jonas_merkle@ymail.com>

--- GameModel.cpp
+++ GameModel.cpp
@@ -8,7 +8,7 @@
 namespace gameModel{
 
     Player::Player(Position position, std::string name, communication::messages::types::Sex gender, communication::messages::types::Broom broom, communication::messages::types::EntityId id) :
-        Object(position, id), name(std::move(name)), gender(gender), broom(broom) {
+        Object(position, id), name(std::move(name)), gender(gender), broom(broom), fined{false} {
         this->isFined = false;
     }
 
