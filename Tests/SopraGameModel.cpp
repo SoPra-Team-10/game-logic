b88b6fda2c1d377125b9018815d62dee074f6667
Tim Luchterhand <tim.luchterhand@uni-ulm.de>
2019-05-05 12:23:12 +0200

    tests noch etwas erweiter

--- Tests/SopraGameModel.cpp
+++ Tests/SopraGameModel.cpp
@@ -132,7 +132,9 @@ TEST(env_test, getOpponents) {
 TEST(env_tests, getSurroundingPositions){
     using env = gameModel::Environment;
     using P = gameModel::Position;
-    for(const auto &pos : env::getSurroundingPositions({10, 2})){
+    auto positions = env::getSurroundingPositions({10, 2});
+    EXPECT_EQ(positions.size(), 8);
+    for(const auto &pos : positions){
         EXPECT_THAT(pos, testing::AnyOf(P{10, 1}, P{9, 1}, P{9, 2}, P{9, 3}, P{10, 3}, P{11, 3}, P{11, 2}, P{11, 1}));
     }
 }
@@ -140,7 +142,9 @@ TEST(env_tests, getSurroundingPositions){
 TEST(env_tests, getSurroundingPositions_corner){
     using env = gameModel::Environment;
     using P = gameModel::Position;
-    for(const auto &pos : env::getSurroundingPositions({2, 1})){
+    auto positions = env::getSurroundingPositions({2, 1});
+    EXPECT_EQ(positions.size(), 5);
+    for(const auto &pos : positions){
         EXPECT_THAT(pos, testing::AnyOf(P{1, 2}, P{2, 2}, P{3, 2}, P{3, 1}, P{3, 0}));
     }
 }
